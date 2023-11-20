ba18efe49fdf5a2fbe76200b6b7c00bcfe87bf60
jmarenin <76976999+jmarenin@users.noreply.github.com>
2023-11-19 19:45:40 -0800

    Print out words that can be made from dictionary

--- GenerateWords.py
+++ GenerateWords.py
@@ -11,7 +11,25 @@ def generate_dict_from_word(word: str):
         word_dict[c.lower()] += 1
     return word_dict
 
+def compare_word_to_dict(word: str, word_dict: defaultdict) -> bool:
+    word_dict = word_dict.copy()
+
+    for c in word:
+        if word_dict[c] == 0:
+            return False
+        else:
+            word_dict[c] -= 1
+
+    return True
+
 
 user_word = input("Enter word: ")
 word_dict = generate_dict_from_word(user_word)
 
+all_words = words.words()
+
+count = 0
+for word in all_words:
+    if compare_word_to_dict(word, word_dict) and len(word) > 2 and word != user_word:
+        count += 1
+        print(f"{count}: {word}")
